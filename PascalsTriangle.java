20e186cf01c0967616c1056a2c77cccfe309f16c
SkillPractical <52999747+skillpractical@users.noreply.github.com>
2020-07-27 00:24:56 -0700

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -18,7 +18,7 @@ public class PascalsTriangle {
 
       for(i = 0; i <= n; i++) {
          for(j = 0; j <= n-i; j++){
-            System.out.println();
+            System.out.print();
          }
          for(j = 0; j <= i; j++){
             System.out.print(" "+ncr(i, j));
