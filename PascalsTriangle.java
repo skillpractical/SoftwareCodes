c49e37a8c22959549a5038e3d6dd448a87e60438
SkillPractical <52999747+skillpractical@users.noreply.github.com>
2020-07-27 00:18:11 -0700

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -12,13 +12,13 @@ public class PascalsTriangle {
       return factorial(n) / ( factorial(n-r) * factorial(r) );
    }
    public static void main(String args[]){
-      System.out.println("Welcome to SkillPractical");
+      System.out.println("");
       int n, i, j;
       n = 5;
 
       for(i = 0; i <= n; i++) {
          for(j = 0; j <= n-i; j++){
-            System.out.println("");
+            System.out.println();
          }
          for(j = 0; j <= i; j++){
             System.out.print(" "+ncr(i, j));
