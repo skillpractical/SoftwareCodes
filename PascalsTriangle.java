9457df27e99f29826462cebbfb67e30b32cf3646
Fikayomi Ayodele <74268576+fikay2003@users.noreply.github.com>
2022-12-18 13:33:16 +0100

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -4,12 +4,14 @@ public class PascalsTriangle {
       int f;
 
       for(f = 1; n > 1; n--){
+         
          f *= n;
       }
       return f;
    }
    static int ncr(int n,int r) {
       return factorial(n) / ( factorial(n-r) * factorial(r) );
+      
    }
    public static void main(String args[]){
       System.out.println("Welcome to SkillPractical Home");
